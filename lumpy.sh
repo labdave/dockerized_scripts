7486dbc74cc8a96584b45bd94edcdd0e041b95ec
Devang <>
2020-04-14 23:04:24 -0400

    add svtyper

--- lumpy.sh
+++ lumpy.sh
@@ -6,8 +6,6 @@ back_distance=$4
 weight=$5
 min_mapping_threshold=$6
 
-echo "${sample}"
-echo "${read_length}"
 scripts="lumpy-sv/scripts"
 # index bam file
 samtools index "${sample}".bam
@@ -35,3 +33,6 @@ lumpy -mw 4 -tt 0 \
 -pe id:sample,bam_file:"${sample}".discordant.sorted.bam,histo_file:"${sample}".lib.histo,mean:$mean,stdev:$std,read_length:"${read_length}",min_non_overlap:"${read_length}",discordant_z:"${discordant_z}",back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
 -sr id:sample,bam_file:"${sample}".split.sorted.bam,back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
 > "${sample}".lumpy.vcf
+
+# post procesing with SVTyper to make GT Calls from the Lumpy vcf  using a Bayesian maximum likelihood algorithm.
+svtyper -B "${sample}".bam -S "${sample}".split.sorted.bam -i "${sample}".lumpy.vcf > "${sample}".gt.vcf
