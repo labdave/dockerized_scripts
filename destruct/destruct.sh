ec7fdad4259110a6479aaf5d724879416d7646cc
Devang <>
2020-04-16 20:39:23 -0400

    add destruct dockerfile

--- destruct/destruct.sh
+++ destruct/destruct.sh
@@ -1,11 +1,10 @@
 # obtain parameters
 bam=$1
+sample=$(echo "${bam}" | sed 's/.bam//g')
 cfg=$2
+out_breaks=$3
+out_break_libs=$4
+out_break_reads=$5
 
-destruct_ref_data=/home/naresh_prodduturi/data/HG38/destruct_ref/
-rundir=/home/naresh_prodduturi/data/translocations/Date_1_3_2020_8MB/Delly_ALT/destruct
-echo "$rundir/$i"
-mkdir -p $rundir/$i
-cd $rundir/$i
-input_bam=/home/naresh_prodduturi/data/translocations/Date_1_3_2020_8MB/Delly_ALT/bamfiles/$i.bwamem_alt_snaut.bam
-destruct run  $destruct_ref_data breaks.tsv break_libs.tsv break_reads.tsv --bam_files $input_bam --lib_ids sample.$i --raw_data_dir destruct_raw_data/ --submit local --config $cfg
+destruct_ref_data="/usr/local/bin/destruct_ref/"
+destruct run "${destruct_ref_data}" "${out_breaks}" "${out_break_libs}" "${out_break_reads}" --bam_files "${bam}" --lib_ids "${sample}" --raw_data_dir "${destruct_raw_data}" --submit local
