74940d16e2cca0616db9d15e784edac1b31a8974
Devang <>
2020-04-20 21:04:56 -0400

    debugging

--- lumpy/lumpy.sh
+++ lumpy/lumpy.sh
@@ -13,48 +13,45 @@ gt_vcf=$8
 # locate lumpy scripts directory
 scripts="lumpy-sv/scripts"
 
-# copy file
-cp "${bam}" /usr/local/bin/sample.bam
-cd /usr/local/bin/
-
 # index bam file
-samtools index /usr/local/bin/sample.bam
+samtools index "${bam}"
 echo "sample indexed" 1>&2
 echo "${bam}"
 
 # extract the discordant paired-end alignments.
-samtools view -b -F 1294 /usr/local/bin/sample.bam > /usr/local/bin/sample.discordant.bam
+samtools view -b -F 1294 "${bam}" > sample.discordant.bam
 echo "discordant reads extracted" 1>&2
 
 # extract the split-read alignments
-samtools view -h /usr/local/bin/sample.bam | "${scripts}"/extractSplitReads_BwaMem -i stdin | samtools view -Sb - > /usr/local/bin/sample.split.bam
+samtools view -h "${bam}" | "${scripts}"/extractSplitReads_BwaMem -i stdin | samtools view -Sb - > sample.split.bam
 echo "split reads extracted" 1>&2
 
 # sort both alignments
-samtools sort /usr/local/bin/sample.discordant.bam > /usr/local/bin/sample.discordant.sorted.bam
-samtools index /usr/local/bin/sample.discordant.sorted.bam
+samtools sort sample.discordant.bam > sample.discordant.sorted.bam
+samtools index sample.discordant.sorted.bam
 echo "discordant reads sorted" 1>&2
 
-samtools sort /usr/local/bin/sample.split.bam > /usr/local/bin/sample.split.sorted.bam
-samtools index /usr/local/bin/sample.split.sorted.bam
+samtools sort sample.split.bam > sample.split.sorted.bam
+samtools index sample.split.sorted.bam
 echo "split reads sorted" 1>&2
 
 # generate empirical insert size statistics on each library in the BAM file 
-samtools view /usr/local/bin/sample.bam | tail -n+100000 | "${scripts}"/pairend_distro.py -r "${read_length}" -X 4 -N 10000 -o /usr/local/bin/sample.lib.histo > tmp.out 2>&1
+samtools view "${bam}" | tail -n+100000 | "${scripts}"/pairend_distro.py -r "${read_length}" -X 4 -N 10000 -o sample.lib.histo > tmp.out 2>&1
 mean=`tail -1 tmp.out | cut -f1 | cut -f2 -d ':' | cut -f1 -d '.'`
 std=`tail -1 tmp.out | cut -f2 | cut -f2 -d ':' | cut -f1 -d '.'`
 echo "library statistics generated" 1>&2
 
 echo "$(pwd)"
 ls -lt
+which lumpy
 
 # lumpy call (traditional)
 lumpy -mw 4 -tt 0 \
--pe id:sample,bam_file:/usr/local/bin/sample.discordant.sorted.bam,histo_file:/usr/local/bin/sample.lib.histo,mean:$mean,stdev:$std,read_length:"${read_length}",min_non_overlap:"${read_length}",discordant_z:"${discordant_z}",back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
--sr id:sample,bam_file:/usr/local/bin/sample.split.sorted.bam,back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
+-pe id:sample,bam_file:sample.discordant.sorted.bam,histo_file:sample.lib.histo,mean:$mean,stdev:$std,read_length:"${read_length}",min_non_overlap:"${read_length}",discordant_z:"${discordant_z}",back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
+-sr id:sample,bam_file:sample.split.sorted.bam,back_distance:"${back_distance}",weight:"${weight}",min_mapping_threshold:"${min_mapping_threshold}" \
 > "${lumpy_vcf}"
 echo "lumpy call finished" 1>&2
 
 # post procesing with SVTyper to make GT Calls from the Lumpy vcf  using a Bayesian maximum likelihood algorithm.
-svtyper -B /usr/local/bin/sample.bam -S /usr/local/bin/sample.split.sorted.bam -i "${lumpy_vcf}" > "${gt_vcf}"
+svtyper -B "${bam}" -S sample.split.sorted.bam -i "${lumpy_vcf}" > "${gt_vcf}"
 echo "svtyper call finished" 1>&2
