c67ef072a3de876d9f6cce957b15469f2f3d0f0b
Devang <>
2020-04-25 20:38:25 -0400

    fix typo file name

--- lumpy_merger/Merge_sample_level_Lumpy.py
+++ lumpy_merger/Merge_sample_level_Lumpy.py
@@ -39,7 +39,7 @@ def main():
 	chr_list_all = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY']
 
 	'''Output  file'''
-	myfile = open(output_file, mode='wt')
+	myfile = open(temp_file, mode='wt')
 	
 	'''Header'''
 	myfile.write("dave_lab_id\t")
