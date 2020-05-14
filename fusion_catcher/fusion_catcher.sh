8e902d1e4a67e4023633adff23db91a85fae4e0e
Devang <>
2020-05-14 15:06:10 -0400

    edit file location

--- fusion_catcher/fusion_catcher.sh
+++ fusion_catcher/fusion_catcher.sh
@@ -2,4 +2,4 @@ input_dir=$1
 output_dir=$2
 data_dir=$3
 
-fusioncatcher.py -i "${input_dir}" -o "${output_dir}" -d "${data_dir}"
+/opt/fusioncatcher/bin/fusioncatcher.py -i "${input_dir}" -o "${output_dir}" -d "${data_dir}"
