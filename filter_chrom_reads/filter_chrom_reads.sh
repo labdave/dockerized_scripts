7768850fe99cdfd811c04b357bfcc3f58d57695e
Devang <devang01121995@gmail.com>
2020-06-04 21:24:52 -0400

    increase number of reads

--- filter_chrom_reads/filter_chrom_reads.sh
+++ filter_chrom_reads/filter_chrom_reads.sh
@@ -5,6 +5,6 @@ new_bam=$4
 temp_bam1=$5
 temp_bam2=$6
 samtools view -h -F ${flag} ${old_bam} | awk '$7!="="' | samtools view -b -@ ${threads} -S - > ${temp_bam1}
-samtools view -h ${old_bam} | head -1000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
+samtools view -h ${old_bam} | head -5000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
 samtools merge ${new_bam} ${temp_bam1} ${temp_bam2}
 samtools index ${new_bam}
