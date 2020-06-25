de24d9bdb0356c2c6601aa5ef33bc594fd3cda10
Devang <devang01121995@gmail.com>
2020-06-24 20:23:51 -0400

    debug disco wave

--- anno_merger/Merge_delly_lumpy_destruct_annotation.py
+++ anno_merger/Merge_delly_lumpy_destruct_annotation.py
@@ -659,8 +659,6 @@ def main():
 			linenum = linenum+1
 			write_tmp.write(rw_lst[0]+'__'+rw_lst[1]+"\t"+str(int(rw_lst[2])-50)+"\t"+str(int(rw_lst[2])+50)+"\t"+str(linenum)+"\t"+str(1)+"\n")
 			write_tmp.write(rw_lst[0]+'__'+rw_lst[3]+"\t"+str(int(rw_lst[4])-50)+"\t"+str(int(rw_lst[4])+50)+"\t"+str(linenum)+"\t"+str(2)+"\n")
-			print(rw_lst[0]+'__'+rw_lst[1]+"\t"+str(int(rw_lst[2])-50)+"\t"+str(int(rw_lst[2])+50)+"\t"+str(linenum)+"\t"+str(1))
-			print(rw_lst[0]+'__'+rw_lst[3]+"\t"+str(int(rw_lst[4])-50)+"\t"+str(int(rw_lst[4])+50)+"\t"+str(linenum)+"\t"+str(2))
 		read_input.close()
 		write_tmp.close()
 		
@@ -674,7 +672,6 @@ def main():
 			rw_lst = line.split("\t")
 			linenum = linenum+1
 			write_tmp.write(rw_lst[0]+'__'+rw_lst[1]+"\t"+str(int(rw_lst[2])-1)+"\t"+rw_lst[3]+"\t"+line+"\n")
-			print(rw_lst[0]+'__'+rw_lst[1]+"\t"+str(int(rw_lst[2])-1)+"\t"+rw_lst[3]+"\t"+line)
 		read_input.close()
 		write_tmp.close()
 		
@@ -690,6 +687,7 @@ def main():
 			rw_lst = line.split("\t")
 			print(rw_lst)
 			if int(rw_lst[-1]) > 0:
+				print('yes')
 				dict_det_trans[rw_lst[0]+' '+rw_lst[2]]=rw_lst[9]+"\t"+rw_lst[10]+"\t"+rw_lst[11]+"\t"+rw_lst[12]+"\t"+rw_lst[13]+"\t"+rw_lst[14]
 		read_rach_out.close()
 		
