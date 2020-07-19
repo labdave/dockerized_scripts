b4e835cf1d234cb92171ff397a7ab70f36bf069b
Devang <devang01121995@gmail.com>
2020-07-19 18:15:04 -0400

    add echos

--- filter_chrom_reads/filter_chrom_reads.sh
+++ filter_chrom_reads/filter_chrom_reads.sh
@@ -5,11 +5,23 @@ new_bam=$4
 temp_bam1=$5
 temp_bam2=$6
 rm ${new_bam}
+echo "started filtering by flag"
 samtools view -@ ${threads} -h -F ${flag} ${old_bam} | awk '$7!="="' | samtools view -b -@ ${threads} -S - > ${temp_bam1}
-samtools view -@ ${threads} -h ${old_bam} | head -100000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
+echo "ended filtering by flag"
 
+echo "started filtering for good reads"
+samtools view -@ ${threads} -h ${old_bam} | head -100000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
+echo "ended filtering for good reads"
+
+echo "started filtering for non-primary reads"
 # add non-primary alignments
 samtools view -@ ${threads} -h -f 256 ${old_bam} | samtools view -Sb -@ ${threads} - > non-primary.bam 
+echo "ended filtering for non-primary reads"
 
+echo "started merging"
 samtools merge -@ ${threads} ${new_bam} ${temp_bam1} ${temp_bam2} non-primary.bam
+echo "ended merging"
+
+echo "started indexing"
 samtools index -@ ${threads} ${new_bam}
+echo "ended indexing"
