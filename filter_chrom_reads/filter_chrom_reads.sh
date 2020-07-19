19dbbf20eddcb3d39d54f7d418fa27b33a6de4e3
Devang <devang01121995@gmail.com>
2020-07-19 18:08:40 -0400

    add non primary alignments

--- filter_chrom_reads/filter_chrom_reads.sh
+++ filter_chrom_reads/filter_chrom_reads.sh
@@ -7,5 +7,9 @@ temp_bam2=$6
 rm ${new_bam}
 samtools view -@ ${threads} -h -F ${flag} ${old_bam} | awk '$7!="="' | samtools view -b -@ ${threads} -S - > ${temp_bam1}
 samtools view -@ ${threads} -h ${old_bam} | head -100000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
-samtools merge -@ ${threads} ${new_bam} ${temp_bam1} ${temp_bam2}
+
+# add non-primary alignments
+samtools view -@ ${threads} -h -f 256 ${old_bam} | samtools view -Sb -@ ${threads} - > non-primary.bam 
+
+samtools merge -@ ${threads} ${new_bam} ${temp_bam1} ${temp_bam2} non-primary.bam
 samtools index -@ ${threads} ${new_bam}
