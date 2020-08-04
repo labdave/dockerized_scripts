fb8adf93e1ab3bb0359ff098170f55140319dfcc
Devang <devang01121995@gmail.com>
2020-08-03 21:29:26 -0400

    update script

---
+++ example_scripts/script.sh
@@ -0,0 +1,18 @@
+# this is a simple script that counts the number of lines in a bam header
+
+bam=""
+while getopts ":b::" opt; do
+  case $opt in
+    b) bam="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+if [ "$bam" != "" ]
+then
+	samtools view -H $bam | wc -l
+else
+	echo "Hello, we are in script.sh"
+fi
