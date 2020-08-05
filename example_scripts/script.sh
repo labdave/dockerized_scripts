d18d930d93a33eae2bc58ddc6986361ef94ac5b4
Devang <devang01121995@gmail.com>
2020-08-05 10:45:16 -0400

    update script

--- example_scripts/script.sh
+++ example_scripts/script.sh
@@ -2,21 +2,21 @@
 
 bam=""
 while getopts ":b::l::c::" opt; do
-  case $opt in
-    b) bam="$OPTARG"
-    ;;
-    l) lines="$OPTARG"
-    ;;
-    c) chars="$OPTARG"
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
+    case $opt in
+        b) bam="$OPTARG"
+        ;;
+        l) lines="$OPTARG"
+        ;;
+        c) chars="$OPTARG"
+        \?) echo "Invalid option -$OPTARG" >&2
+        ;;
+    esac
 done
 
 if [ "$bam" != "" ] && [ "$lines" != "" ] && [ "$chars" != "" ]
 then
-	samtools view -H $bam | wc -l > $lines
-  samtools view -H $bam | wc -m > $chars
+    samtools view -H $bam | wc -l > $lines
+    samtools view -H $bam | wc -m > $chars
 else
-	echo "Hello from inside script.sh"
+    echo "Hello from inside script.sh"
 fi
