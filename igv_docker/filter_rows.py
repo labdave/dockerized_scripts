2921938a61858a1e42746a3b459fc83e1ba58350
Devang <devang01121995@gmail.com>
2020-08-05 17:10:28 -0400

    fix thresh issue

--- igv_docker/filter_rows.py
+++ igv_docker/filter_rows.py
@@ -21,6 +21,8 @@ with open(args.input, 'r') as f:
 		if args.thresh_column is not None and args.thresh is not None:
 			if int(line_arr[int(args.thresh_column)]) > int(args.thresh):
 				new_str += '\t'.join(line_arr[:int(args.columns)])+'\n'
+		else:
+			new_str += '\t'.join(line_arr[:int(args.columns)])+'\n'
 
 with open(args.output, 'w') as f:
 	f.write(new_str)
