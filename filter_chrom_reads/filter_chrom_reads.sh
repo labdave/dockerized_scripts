4379098f36016a663b1d990baef9dfe9683cf5d6
Devang <devang01121995@gmail.com>
2020-08-06 13:31:07 -0400

    add time logging

--- filter_chrom_reads/filter_chrom_reads.sh
+++ filter_chrom_reads/filter_chrom_reads.sh
@@ -8,29 +8,30 @@ padded_bed=$7
 rm ${new_bam}
 
 echo "started filtering for on-target reads"
-samtools view -@ ${threads} -b -L ${padded_bed} ${old_bam} | cut -d'	' -f1 > tmp
-sort -u -S20G --parallel ${threads} tmp > on-target.reads.txt
-java -jar picard.jar FilterSamReads I=${old_bam} O=on-target.bam RLF=on-target.reads.txt FILTER=includeReadList
+time samtools view -@ ${threads} -b -L ${padded_bed} ${old_bam} | cut -d'	' -f1 > tmp
+time sort -u -S20G --parallel ${threads} tmp > on-target.reads.txt
+time java -jar picard.jar FilterSamReads I=${old_bam} O=on-target.bam RLF=on-target.reads.txt FILTER=includeReadList
+echo "ended filtering for on-target reads"
 
 echo "started filtering by flag"
-samtools view -@ ${threads} -h -F ${flag} on-target.bam | awk '$7!="="' | samtools view -b -@ ${threads} -S - > ${temp_bam1}
+time samtools view -@ ${threads} -h -F ${flag} on-target.bam | awk '$7!="="' | samtools view -b -@ ${threads} -S - > ${temp_bam1}
 echo "ended filtering by flag"
 
 echo "started filtering for good reads"
-samtools view -@ ${threads} -h on-target.bam | head -100000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
+time samtools view -@ ${threads} -h on-target.bam | head -100000 | samtools view -b -@ ${threads} -S - > ${temp_bam2}
 echo "ended filtering for good reads"
 
 echo "started filtering for non-primary reads"
 # add non-primary alignments
-samtools view -@ ${threads} -f 256 on-target.bam | cut -d'	' -f1 > tmp
-sort -u -S20G --parallel ${threads} tmp > non-primary.reads.txt
-java -jar picard.jar FilterSamReads I=on-target.bam O=non-primary.bam RLF=non-primary.reads.txt FILTER=includeReadList
+time samtools view -@ ${threads} -f 256 on-target.bam | cut -d'	' -f1 > tmp
+time sort -u -S20G --parallel ${threads} tmp > non-primary.reads.txt
+time java -jar picard.jar FilterSamReads I=on-target.bam O=non-primary.bam RLF=non-primary.reads.txt FILTER=includeReadList
 echo "ended filtering for non-primary reads"
 
 echo "started merging"
-samtools merge -cp -@ ${threads} ${new_bam} ${temp_bam1} ${temp_bam2} non-primary.bam
+time samtools merge -cp -@ ${threads} ${new_bam} ${temp_bam1} ${temp_bam2} non-primary.bam
 echo "ended merging"
 
 echo "started indexing"
-samtools index -@ ${threads} ${new_bam}
+time samtools index -@ ${threads} ${new_bam}
 echo "ended indexing"
