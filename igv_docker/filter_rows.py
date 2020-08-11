3e0b558119913c4f42db4f1555edd827a946bd3b
Devang <devang01121995@gmail.com>
2020-08-11 17:45:07 -0400

    filter for 3 callers

--- igv_docker/filter_rows.py
+++ igv_docker/filter_rows.py
@@ -23,6 +23,7 @@ with open(args.input, 'r') as f:
 			if int(line_arr[int(args.thresh_column)]) > int(args.thresh):
 				if line_arr[1] in ['chr3', 'chr8', 'chr18'] or line_arr[3] in ['chr3', 'chr8', 'chr18']:
 					# also filter for three callers
+					print(line_arr[68])
 					if int(line_arr[68]) == 3:
 						new_str += '\t'.join(line_arr[:int(args.columns)])+'\n'
 		else:
