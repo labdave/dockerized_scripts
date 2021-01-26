a0d73c24f80dbec01ef356cc39c06f7014ec03fe
Rachel Kositsky <rachel.kositsky@duke.edu>
2021-01-26 17:34:11 -0500

    Add explicit parsing and logging for sv_pon creation

--- sv_pon/create_translocation_pon.R
+++ sv_pon/create_translocation_pon.R
@@ -35,10 +35,56 @@ min_reads = opt$min_reads
 stopifnot(min_samples <= length(input_files_list))
 
 #### Download all translocation positions ####
-trl_df <- read_tsv(input_files_list[1])
-if (length(input_files_list) > 1) {
-  for (i in 2:length(input_files_list)) {
-    trl_df <- rbind(trl_df, read_tsv(input_files_list[i]))
+
+parse_translocation_tsv <- function(path) {
+  return(read_tsv(path,
+                  col_types = 
+                    cols(
+                      dave_lab_id = col_character(),
+                      chr1 = col_character(),
+                      pos1 = col_double(),
+                      chr2 = col_character(),
+                      pos2 = col_double(),
+                      pe = col_double(),
+                      sr = col_double(),
+                      pe_sr = col_double(),
+                      Num_callers = col_double(),
+                      Callers = col_character(),
+                      discowave_chrom = col_character(),
+                      discowave_start = col_double(),
+                      discowave_stop = col_double(),
+                      discowave_pe = col_double(),
+                      discowave_depth = col_double(),
+                      discowave_pct_to_partner = col_double(),
+                      discowave_partner_chrom = col_character(),
+                      discowave_evenness = col_double(),
+                      BP1_repeats_200bp = col_character(),
+                      BP2_repeats_200bp = col_character(),
+                      matching_repeats = col_character(),
+                      BP1_polynt_200bp = col_character(),
+                      BP2_polynt_200bp = col_character(),
+                      BP1_segdup_200bp = col_character(),
+                      BP2_segdup_200bp = col_character(),
+                      segdup_100k = col_character(),
+                      segdup_1M = col_character(),
+                      segdup_10M = col_character(),
+                      segdup_100M = col_character(),
+                      Ig = col_character(),
+                      FISH_capture = col_character(),
+                      PON = col_double())
+  ))
+}
+
+trl_df <- parse_translocation_tsv(input_files_list[1])
+stop_for_problems(trl_df)
+n_files <- length(input_files_list)
+print(sprintf("Parsed %s [1/%d]", input_files_list[1], n_files))
+
+if (n_files > 1) {
+  for (i in 2:n_files) {
+    trl_df <- rbind(trl_df, parse_translocation_tsv(input_files_list[i]))
+    stop_for_problems(trl_df)
+    print(sprintf("Parsed %s [%d/%d]", input_files_list[i], i, n_files))
   }
 }
 
@@ -60,14 +106,33 @@ df <- df %>% arrange(sample_id) %>%
   arrange(pos2) %>% arrange(match(chr2, chrom_order)) %>% 
   arrange(pos1) %>% arrange(match(chr1, chrom_order))
 
+print("Created normal dataframe; now collapsing windows...")
+
 # Collapse windows ####
 # Copy over first window
 output_df <- df[1,]
+
+# Logging to retain sanity in long runs
+log_points = ceiling(1:10 * nrow(df) / 10)
 
 # We assume that the input windows are sorted by sample and chromosomal position.
 for(i in 2:nrow(df)) {
   j <- nrow(output_df)
   
+  # Print log message every 10%
+  if (i %in% log_points) {
+    print(sprintf("%d%% done.. [%d/%d rows]", 
+                  which(log_points == i)*10, i, nrow(df)))
+  } 
+  
+  # else if (i == ceiling(nrow(df)*2/10)) {
+  #   print(sprintf("20%% done.. [%d/%d rows]", i, nrow(df)))
+  # } else if (i == ceiling(nrow(df)/2)) {
+  #   print(sprintf("50%% done.. [%d/%d rows]", i, nrow(df)))
+  # } else if (i == ceiling(nrow(df)*3/4)) {
+  #   print(sprintf("75%% done.. [%d/%d rows]", i, nrow(df)))
+  # }
+  # 
   if ((df[i, "chr1"] == output_df[j, "chr1"]) &&
       ((df[i, "pos1"] - output_df[j, "pos1"]) <= merge_distance) &&
       (df[i, "chr2"] == output_df[j, "chr2"]) &&
@@ -99,8 +164,12 @@ output_df$n_samples <- str_count(output_df$sample_id, pattern = ",") + 1
 
 # Save output
 write_delim(output_df, file = unfiltered_pon, delim = "\t")
+print(sprintf("Wrote unfiltered output at %s", unfiltered_pon))
 
 # Save filtered output filtered on # samples and the most reads seen in a normal
 write_delim(output_df %>% filter(n_samples >= min_samples) %>% filter(pe_sr >= min_reads), 
             file = filtered_pon, delim = "\t")
+print(sprintf("Wrote filtered output (%d+ samples, %d+ reads) at %s",
+              min_samples, min_reads, filtered_pon))
 
+print("Done!")
