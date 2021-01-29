ed2f4709e6f3b15e06e15f10409389348be0ebb6
Devang <devang01121995@gmail.com>
2021-01-28 22:37:33 -0500

    debug

--- cnv_merger/merge_tables.py
+++ cnv_merger/merge_tables.py
@@ -7,6 +7,8 @@ import sys
 input_files = sys.argv[1].strip().split("|")
 output_file = sys.argv[2]
 
+print(input_files)
+print(output_file)
 
 template_file = input_files.pop(0)
 with open(template_file, "r") as f:
@@ -15,11 +17,17 @@ with open(template_file, "r") as f:
 		mode = header[0]
 		break
 
+print(mode)
+
 df = pd.read_csv(input_files[0], sep="\t", index_col=mode)
+
+print(df)
 
 big_df = df
 for input_file in input_files:
 	df1 = pd.read_csv(input_file, sep="\t", index_col=mode)
 	big_df = pd.concat([df, df1], axis=1)
+
+print(big_df)
 
 big_df.to_csv(output_file, sep="\t")
