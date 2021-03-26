5a49efbb747414956efba6ee155608316e75fa8b
Devang <devang01121995@gmail.com>
2021-03-25 20:16:09 -0400

    fix bugs

--- fusion_aggregator/Whitelist_fusions.py
+++ fusion_aggregator/Whitelist_fusions.py
@@ -11,11 +11,14 @@ wl_coords = []
 with open(whitelist, "r") as f:
 	for line in f:
 		line_arr = line.strip().split()
-		wl_coords.append([line_arr[0], line_arr[1], line_arr[2], line_arr[3]])
+		wl_coords.append([line_arr[0], int(line_arr[1]), int(line_arr[2]), line_arr[3]])
 
 wl_genes = []
 with open(input_file, "r") as f:
 	for line in f:
+		# skip header
+		if "breakpoint" in line:
+			continue
 		line_arr = line.strip().split()
 		bp_arr = []
 		bp_arr += line_arr[4].split(",")
@@ -23,13 +26,14 @@ with open(input_file, "r") as f:
 		for bp in bp_arr:
 			for wl_coord in wl_coords:
 				chrom = bp.split(":")[0]
-				pos = bp.split(":")[1]
-				if (chrom == wl_coords[0]) and (pos > wl_coords[1]) and (pos < wl_coords[2]):
-					wl_genes.append(wl_coords[4])
+				pos = int(bp.split(":")[1])
+				if (chrom == wl_coord[0]) and (pos > wl_coord[1]) and (pos < wl_coord[2]):
+					wl_genes.append(wl_coord[3])
 
 
 wl_genes = list(set(wl_genes))
 
+print(wl_genes)
 with open(wl_fusions_file, "w") as f:
 	for gene in wl_genes:
 		f.write(gene+"\n")
