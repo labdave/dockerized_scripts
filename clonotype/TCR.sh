4f30d9ff61258b8529ffd40057177f7441ac3b93
devang <devang01121995@gmail.com>
2021-07-13 17:42:47 -0400

    update remotes and tcr

--- clonotype/TCR.sh
+++ clonotype/TCR.sh
@@ -20,28 +20,54 @@ echo "========="
 echo
 
 # get index
+# echo "indexing started"
 #samtools index -@ $threads "$sample"
+# sleep 5
+# echo "indexing done"
 
 # get unmapped reads
+echo "unmapped started"
 samtools view -b -h -f 4 -@ $threads "$sample" > "$sample".unmapped.bam
+sleep 5
+echo "unmapped done"
 
 # get TRA reads
+echo "TRA started"
 samtools view -b -h -@ $threads "$sample" "chr14:21621904-22552132" > "$sample".TRA.bam
+sleep 5
+echo "TRA done"
 
 # get TRB reads
+echo "TRB started"
 samtools view -b -h -@ $threads "$sample" "chr7:142299011-142813287" > "$sample".TRB.bam
+sleep 5
+echo "TRB done"
 
 # get TRG reads
+echo "TRG started"
 samtools view -b -h -@ $threads "$sample" "chr7:38240024-38368055" > "$sample".TRG.bam
+sleep 5
+echo "TRG done"
 
 # get TRD reads
+echo "TRG started"
 samtools view -b -h -@ $threads "$sample" "chr14:22422546-22466577" > "$sample".TRD.bam
+sleep 5
+echo "TRG done"
 
 # merge TR reads
+echo "merge started"
 samtools merge -f -@ $threads "$sample".TR.bam "$sample".TRA.bam "$sample".TRB.bam "$sample".TRG.bam "$sample".TRD.bam
+sleep 5
+echo "merge done"
 
 # sort TR reads
+echo "sort started"
 samtools sort -@ $threads -n "$sample".TR.bam "$sample".TR.sorted
+sleep 5
+echo "sort done"
+
+echo "more processing started"
 
 # sort unmapped reads
 samtools sort -n "$sample".unmapped.bam "$sample".unmapped.sorted
@@ -56,22 +82,36 @@ bedtools bamtofastq -i "$sample".TR.sorted.bam -fq "$sample".TR.R1.fastq -fq2 "$
 cat "$sample".unmapped.R1.fastq "$sample".TR.R1.fastq > "$sample".combined.T.R1.fastq
 cat "$sample".unmapped.R2.fastq "$sample".TR.R2.fastq > "$sample".combined.T.R2.fastq
 
+sleep 5
+echo "more processing done"
+
+echo "mixcr started"
+
+echo "mixcr align"
 # mixcr align
 mixcr align -f -r "$report" -s human -t $threads -p rna-seq -OallowPartialAlignments=true -OvParameters.geneFeatureToAlign=VGeneWithP "$sample".combined.T.R1.fastq "$sample".combined.T.R2.fastq "$sample".T.vdjca
 
+echo "mixcr assemblePartial"
 # mixcr assemblePartial
 mixcr assemblePartial -f "$sample".T.vdjca "$sample".T.rescued.vdjca
 mixcr assemblePartial -f "$sample".T.rescued.vdjca "$sample".T.rescued2.vdjca
 
+echo "mixcr assemble"
 # mixcr assemble
 mixcr assemble -f "$sample".T.rescued2.vdjca "$sample".T.clns
 
+echo "mixcr exportClones"
 # mixcr exportClones
 mixcr exportClones -count -fraction -vGene -dGene -jGene -vAlignment -dAlignment -jAlignment -aaFeature CDR3 "$sample".T.clns "$sample".T.clones.tsv
 # mixcr exportClones -f -o -t "$sample".T.clns "$sample".T.clones.tsv
 
+sleep 5
+echo "mixcr done"
+
+echo "parse output started"
+
 # parse output
-cat "$sample".T.clones.tsv | grep -v 'IGH' | grep -v 'IGK' | grep -v 'IGL' > "$all_output"
+cat "$sample".T.clones.tsv | grep -v 'IGH' | grep -v 'IGK' | grep -v 'IGK' > "$all_output"
 # get header
 head -1 "$all_output" > "$sample".T.clones.tsv.head1
 # add sample info header
@@ -93,10 +133,9 @@ grep -m 1 "TRD" "$all_output" > "$sample".T.clones.tsv.TRD
 # add sample info
 sed -i -e "s/^/$id	/" "$sample".T.clones.tsv.TRD
 # cat all files together
-grep -m 1 "TRA" "$all_output" > "$sample".T.clones.tsv.TRA
-grep -m 1 "TRB" "$all_output" > "$sample".T.clones.tsv.TRB
-grep -m 1 "TRG" "$all_output" > "$sample".T.clones.tsv.TRG
-grep -m 1 "TRD" "$all_output" > "$sample".T.clones.tsv.TRD
 cat "$sample".T.clones.tsv.head1 "$sample".T.clones.tsv.TRA "$sample".T.clones.tsv.TRB "$sample".T.clones.tsv.TRG "$sample".T.clones.tsv.TRD > "$top_output"
-# cut -f 2,3,4,6,7,8,9 "$sample".T.clones.tsv.filter2 > "$sample".T.clones.tsv.filter3
-# sed 's/[(][^)]*[)]//g' "$sample".T.clones.tsv.filter3 > "$output"
+# cut -f 2,3,4,6,7,8,9 "$sample".IG.clones.tsv.filter2 > "$sample".IG.clones.tsv.filter3
+# sed 's/[(][^)]*[)]//g' "$sample".IG.clones.tsv.filter3 > "$output"
+
+sleep 5
+echo "parse output done"
