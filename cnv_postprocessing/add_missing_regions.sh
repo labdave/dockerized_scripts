a3bb8b100cca178c08592e2f5cbfc80597ef51fc
devang <devang01121995@gmail.com>
2021-09-28 00:56:03 -0400

    debug

--- cnv_postprocessing/add_missing_regions.sh
+++ cnv_postprocessing/add_missing_regions.sh
@@ -1,15 +1,14 @@
 # remove header
-sed -i '/chrom/d' $1
-head $1
+awk -v FS='	' -v OFS='	' '{print $2,$3,$4,$5,$6,$1}' $1 | sed -i '/chrom/d' > $1.swp
 
 # get complement
-bedtools complement -i $1 -g hg38.fa.fai > $1.comp
+bedtools complement -i $1.swp -g hg38.fa.fai > $1.comp
 
 # add 0 cnv and sample id for complement regions
 awk -v FS='	' -v OFS='	' -v ref=$3 '{print $1,$2+1,$3-1,0,0,ref}' $1.comp > tmp
 
 # combine both files
-cat tmp $1 > tmp1
+cat tmp $1.swp > tmp1
 
 # sort combined file
 bedtools sort -i tmp1 > tmp
