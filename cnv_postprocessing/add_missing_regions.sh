b11fd0c831a3a0465b09cc7c80c698f5f23a14e6
devang <devang01121995@gmail.com>
2021-09-28 00:56:34 -0400

    debug

--- cnv_postprocessing/add_missing_regions.sh
+++ cnv_postprocessing/add_missing_regions.sh
@@ -1,5 +1,5 @@
 # remove header
-awk -v FS='	' -v OFS='	' '{print $2,$3,$4,$5,$6,$1}' $1 | sed -i '/chrom/d' > $1.swp
+awk -v FS='	' -v OFS='	' '{print $2,$3,$4,$5,$6,$1}' $1 | sed '/chrom/d' > $1.swp
 
 # get complement
 bedtools complement -i $1.swp -g hg38.fa.fai > $1.comp
