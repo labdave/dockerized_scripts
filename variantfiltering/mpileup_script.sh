d2634f9ee9052fb3deee9e32d9c57e5c3a96968c
devang <devang01121995@gmail.com>
2021-09-30 11:35:11 -0400

    update ref

--- variantfiltering/mpileup_script.sh
+++ variantfiltering/mpileup_script.sh
@@ -8,11 +8,12 @@ mpileup_vars=$4
 sample_id=$5
 depth_thresh=$6
 std_thresh=$7
+ref=$8
 
 # remove headers and get column names
 grep -v 'CHROM_POS_REF_ALT' $clean_vars | cut -f2,3 > all_calls.txt
 
 # run samtools mpileup
-samtools mpileup --output-BP -a -f ref.fa -l all_calls.txt $bam > all_vars.txt
+samtools mpileup --output-BP -a -f $ref -l all_calls.txt $bam > all_vars.txt
 
 python3 add_mpileup_af.py all_vars.txt $clean_vars $depth_thresh $std_thresh $mpileup_vars
