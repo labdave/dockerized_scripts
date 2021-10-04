9943438abe43e8ed168edc08be3972c544f7c201
devang <devang01121995@gmail.com>
2021-10-04 18:42:38 -0400

    debug

--- variantfiltering/single_sample_VCF_merge_and_filter.R
+++ variantfiltering/single_sample_VCF_merge_and_filter.R
@@ -101,7 +101,7 @@ clean_filtered<-all_filt_variants
 clean_filtered <- clean_filtered[clean_filtered$Func.refGene=="exonic",]        
 clean_filtered <- clean_filtered[!(clean_filtered$ExonicFunc.refGene=="synonymous_SNV"),]
 
-all_depth<-clean_filtered[,grep("*_AD$", colnames(clean_filtered))]
+all_depth<-clean_filtered[,grep(".*_AD$", colnames(clean_filtered))]
 alt_depth<-matrix(NA, nrow=nrow(all_depth), ncol=ncol(all_depth))
 for(i in 1:nrow(all_depth)){
         for(j in 1:ncol(all_depth)){
