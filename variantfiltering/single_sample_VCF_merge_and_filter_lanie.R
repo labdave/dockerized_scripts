918911f8912ac0dc6eef74585813f6450b8ca93b
lanieehapp <lanieehapp@gmail.com>
2022-02-09 09:21:30 -0800

    added non-WHO filter options

--- variantfiltering/single_sample_VCF_merge_and_filter_lanie.R
+++ variantfiltering/single_sample_VCF_merge_and_filter_lanie.R
@@ -20,7 +20,7 @@ ref=args[6]
 ref_fai=args[7]
 
 
-source("single_sample_VCF_merge_functions_popfreq1pct.R")
+source("single_sample_VCF_merge_functions_lanie.R")
 
 single.sample.merged<-three.caller.merge(samp, path)
 
